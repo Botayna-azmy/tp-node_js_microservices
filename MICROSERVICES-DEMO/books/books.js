f7f24559d295d13e6f81aa8fcb6d74e018f0ac42
Botayna-azmy <bottaynaazmy@gmail.com>
2024-04-06 14:32:05 +0000

    add books

--- MICROSERVICES-DEMO/books/books.js
+++ MICROSERVICES-DEMO/books/books.js
@@ -18,3 +18,42 @@ app.post('/book',(req,res)=>{
         res.status(500).send('Internal Server Error!');
     })
 })
+app.get('/book',(req,res)=>{
+    Book.find().then((books) => {
+        if(books.length !==0){
+            res.json(books)
+        }else{
+            res.status(404).send('Books not found!');
+        }
+    }).catch(err => {
+        res.status(500).send('Internal Server Error!');
+    })
+})
+app.get('/book/:id',(req,res)=>{
+    Book.findById(req.params.id).then((books) => {
+        if(book){
+            res.json(book)
+        }else{
+            res.status(404).send('Books not found!');
+        }
+    }).catch(err => {
+        res.status(500).send('Internal Server Error!');
+    })
+})
+
+app.delete('/book/:id', (req, res) => {
+    Book.findOneAndRemove({ _id: req.params.id }).then((book) => {
+      if (book) {
+        res.json('Book deleted Successfully!');
+      } else {
+        res.status(404).send('Book Not found!');
+      }
+    }).catch((err) => {
+      res.status(500).send('Internal Server Error!');
+    });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Up and Running on port ${port} - This is Book service`);
+  });
+
